9b202d59884394ba6148abc8620b34b6f9bf09bd
Carvidya Nerlita <carvidyanerlita@smkwikrama.sch.id>
2026-05-12 15:14:49 +0700

    css dikit

---
+++ js/main.js
@@ -0,0 +1,31 @@
+// menu data
+const Menu = [
+    {id:1 , name: es-jeruk, harga: , gambar: URL(Img/Baso.png)},
+    {id:2 , name: kopi, harga: , gambar: },
+    {id:3 , name: es-coklat, harga: , gambar: },
+    {id:4 , name: , harga: , gambar: },
+    {id:5 , name: , harga: , gambar: },
+    {id:6 , name: , harga: , gambar: },
+    {id:7 , name: , harga: , gambar: },
+    {id:8 , name: , harga: , gambar: },
+    {id:9 , name: , harga: , gambar: },
+    {id:10 , name: , harga: , gambar: },
+    {id:11 , name: , harga: , gambar: },
+    {id:12 , name: , harga: , gambar: },
+    {id:13 , name: , harga: , gambar: },
+    {id:14 , name: , harga: , gambar: },
+];
+
+let belanjaan = []
+
+const searchBar = document.getElementById("searchBar")
+const hasilPencarian = document.getElementById("hasilPencarian")
+
+function keranjangBelanja(){
+
+}
+
+// search makanan
+function filter(){
+    return Menu.filter()
+}
