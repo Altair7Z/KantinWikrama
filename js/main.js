738801f3628dde1b5f83f20d18c27f217f515068
Carvidya Nerlita <carvidyanerlita@smkwikrama.sch.id>
2026-05-25 14:21:02 +0700

    foto js

--- js/main.js
+++ js/main.js
@@ -4,85 +4,92 @@ const Menu = [
         id:1 , 
         name: es-jeruk, 
         harga: 5, 
-        gambar: url("Img/Baso.png")
+        gambar: url("Img/Es-jeruk.png")
     },
     {
         id:2 , 
         name: kopi, 
-        harga: , 
-        gambar: 
+        harga: 5, 
+        gambar: url("Img/Es-Kopi2.png")
     },
     {
         id:3 , 
         name: es-coklat, 
-        harga: , 
-        gambar: 
+        harga: 5, 
+        gambar: url ("Img/Es-Kopi1.png")
     },
     {
         id:4 , 
-        name: , 
-        harga: , 
-        gambar: 
+        name: jus-jambu, 
+        harga: 5, 
+        gambar: url ("Img/JusJambu.png")
     },
     {
         id:5 , 
-        name: , 
-        harga: , 
-        gambar: 
+        name: es-kulkul, 
+        harga: 2, 
+        gambar: url ("Img/eskulkul.jpg")
     },
     {
         id:6 , 
-        name: , 
-        harga: , 
-        gambar: 
+        name: nanas, 
+        harga: 2, 
+        gambar: url ("Img/nanas.jpg")
     },
     {
         id:7 , 
-        name: , 
-        harga: , 
-        gambar: 
+        name: semangka, 
+        harga: 2, 
+        gambar: url ("Img/semangka.webp")
     },
     {
         id:8 , 
-        name: , 
-        harga: , 
-        gambar: 
+        name: risol-coklat, 
+        harga: 3, 
+        gambar: url ("Img/RisolCoklat.png")
     },
     {
         id:9 , 
-        name: , 
-        harga: , 
-        gambar: 
+        name: risol-matcha, 
+        harga: 3, 
+        gambar: url ("Img/risolmatcha.jpg")
     },
     {
         id:10 , 
-        name: , 
-        harga: , gambar: 
+        name: pie-susu, 
+        harga: 3, 
+        gambar: url ("Img/PieSusu.jpg")
     },
     { 
         id:11 , 
-        name: ,
-        harga: , 
-        gambar: 
+        name: piscok,
+        harga: 2, 
+        gambar: url ("Img/piscok.jpg")
     },
     {
         id:12 , 
-        name: , 
-        harga: , 
-        gambar: 
+        name: sosis, 
+        harga: 5, 
+        gambar: url ("Img/sosisbakar.png")
     },
     {
         id:13 , 
-        name: ,     
-        harga: , 
-        gambar: 
+        name: nasi-bakar,     
+        harga: 8, 
+        gambar: url ("Img/nasibakar.jpg")
     },
     {
         id:14 , 
-        name: , 
-        harga: , 
-        gambar: 
+        name: Baso, 
+        harga: 10, 
+        gambar: url ("Img/Baso.png")
     },
+    {
+    id:15 , 
+    name: mie, 
+    harga: 10, 
+    gambar: url ("Img/MieAyam.png")
+},
 ];
 
 let belanjaan = []
